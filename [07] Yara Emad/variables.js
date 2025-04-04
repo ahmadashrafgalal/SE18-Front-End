6f60664a7e58a9f7b188fa976fd46189beb9de80
Yara Emad Eldien <143430513+yaraemad4869@users.noreply.github.com>
2025-04-05 01:32:01 +0200

    Variables

---
+++ [07] Yara Emad/variables.js
@@ -0,0 +1,58 @@
+var events = [
+    {
+        "id": 1,
+        "name": "Webinar of AI and its inspects",
+        "date": "Sat. 22 March 2025 12:00PM",
+        "location": "Floor 2, Hall 3",
+        "capacity": 50,
+        "registered": 42
+    },
+    {
+        "id": 2,
+        "name": "Cultural Competition",
+        "date": "Sun. 23 March 2025 12:00PM",
+        "location": "Floor 2, Hall 3",
+        "capacity": 50,
+        "registered": 50
+    },
+    {
+        "id": 3,
+        "name": "First aid session  ",
+        "date": "Thu. 27 March 2025 12:00PM",
+        "location": "Floor 1, Amphitheater 2",
+        "capacity": 200,
+        "registered": 101
+    }
+]
+var users = [
+    {
+        "id": 1,
+        "name": "Ahmed",
+        "userType": "employee"
+    },
+    {
+        "id": 2,
+        "name": "Sayed",
+        "userType": "doctor"
+    },
+    {
+        "id": 3,
+        "name": "Yooora",
+        "userType": "student"
+    }
+]
+var userEvents = [
+    {
+        "userId": 3,
+        "eventId": 1
+    },
+    {
+        "userId": 2,
+        "eventId": 2
+    }
+]
+var currentUser = {
+    "id": 3,
+    "name": "Yooora",
+    "userType": "student"
+}
